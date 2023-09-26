58021ac6ddb5cd341918f81f37bc42ad0528a4a3
Joe Rogan <76793890+joe-rogan-0001@users.noreply.github.com>
2023-09-26 21:03:00 +0100

    1f1

---
+++ [1of1sounds]/cammedcharger/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/cammedcharger_game.dat151.rel',
+  'audioconfig/cammedcharger_sounds.dat54.rel',
+  'sfx/dlc_cammedcharger/cammedcharger.awc',
+  'sfx/dlc_cammedcharger/cammedcharger_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/cammedcharger_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/cammedcharger_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_cammedcharger'
