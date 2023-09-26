761a76c5e4377e976878120d2626d6e9d9b8cef7
Joe Rogan <76793890+joe-rogan-0001@users.noreply.github.com>
2023-09-26 20:50:51 +0100

    a

---
+++ lrp-vipcars/fxmanifest.lua
@@ -0,0 +1,39 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'carcols2.meta',
+    'carcols3.meta',
+    'carcols4.meta',
+    'carcols5.meta',
+    'carcols6.meta',
+    'carcols7.meta',
+    'carcols8.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',
+    'vehiclelayouts2.meta',
+    'vehiclelayouts3.meta',
+    'vehiclelayouts4.meta',
+    'vehiclelayouts5.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'CARCOLS_FILE' 'carcols2.meta'
+data_file 'CARCOLS_FILE' 'carcols3.meta'
+data_file 'CARCOLS_FILE' 'carcols4.meta'
+data_file 'CARCOLS_FILE' 'carcols5.meta'
+data_file 'CARCOLS_FILE' 'carcols6.meta'
+data_file 'CARCOLS_FILE' 'carcols7.meta'
+data_file 'CARCOLS_FILE' 'carcols8.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts2.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts3.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts4.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts5.meta'
