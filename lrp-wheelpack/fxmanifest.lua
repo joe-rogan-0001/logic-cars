7d74a9551fcb2aa35d411942327a3060a50570f5
haiku <94936310+haiku00@users.noreply.github.com>
2023-10-08 21:46:07 +0300

    custom wheels

---
+++ lrp-wheelpack/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+files {
+    'dlctext.meta',
+    'carcols.meta',
+    'jdmrimscontentunlocks.meta',
+}
+
+data_file 'DLC_TEXT_FILE' 'dlctext.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'JDM_RIMS_CONTENT_UNLOCKS_FILE' 'jdmrimscontentunlocks.meta'
