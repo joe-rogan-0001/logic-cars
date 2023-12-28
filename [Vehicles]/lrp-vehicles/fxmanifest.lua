3a6ef19b19975d85e00777973a8b90784cc5bfcf
haiku <94936310+haiku00@users.noreply.github.com>
2023-12-28 02:13:14 +0200

    shi

--- [Vehicles]/lrp-vehicles/fxmanifest.lua
+++ [Vehicles]/lrp-vehicles/fxmanifest.lua
@@ -1,10 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-files {
+--[[files {
     'popgroups.ymt',
     'popcycle.dat'
 }
 
 data_file 'DLC_POP_GROUPS' 'popgroups.ymt'
-data_file 'POPSCHED_FILE' 'popcycle.dat'
+data_file 'POPSCHED_FILE' 'popcycle.dat']]--
